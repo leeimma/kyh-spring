80f32c14c603f73fcd78484f23f112517c641e14
imina <leeimma@naver.com>
2024-04-23 14:10:08 +0900

    [db 1] section 1

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,10 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/projects/h2/tablespace/jdbc";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+
+
+}
